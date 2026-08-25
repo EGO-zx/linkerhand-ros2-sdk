e2911c96ed5b73576d3ca9b560cedc5dc1f15f85
hejianxin79 <hejianxin79@hotmail.com>
2026-08-25 16:47:02 +0800

    支持G20 RS485模式

--- linker_hand_ros2_sdk/linker_hand_ros2_sdk/LinkerHand/linker_hand_api.py
+++ linker_hand_ros2_sdk/linker_hand_ros2_sdk/LinkerHand/linker_hand_api.py
@@ -53,9 +53,13 @@ class LinkerHandApi:
         if self.hand_joint == "L20":
             from core.can.linker_hand_l20_can import LinkerHandL20Can
             self.hand = LinkerHandL20Can(can_id=self.hand_id,can_channel=self.can, yaml=self.yaml)
-        if self.hand_joint == "G20":
-            from core.can.linker_hand_g20_can import LinkerHandG20Can
-            self.hand = LinkerHandG20Can(can_id=self.hand_id,can_channel=self.can, yaml=self.yaml)
+        if self.hand_joint == "G20" or self.hand_joint =="L20_V10" or self.hand_joint =="L20_V11":
+            if modbus != "None":
+                from core.rs485.linker_hand_l20_rs485 import LinkerHandL20RS485
+                self.hand = LinkerHandL20RS485(hand_id=self.hand_id,modbus_port=modbus,baudrate=115200)
+            else:
+                from core.can.linker_hand_g20_can import LinkerHandG20Can
+                self.hand = LinkerHandG20Can(can_id=self.hand_id,can_channel=self.can, yaml=self.yaml)
             time.sleep(0.01)
             self.is_palm_touch = self.hand.get_touch_sensor_type()
             ColorMsg(msg=f"传感器类型:{self.is_palm_touch}")
